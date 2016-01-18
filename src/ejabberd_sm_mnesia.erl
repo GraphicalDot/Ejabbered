8af1e854582104fba3a85f11d865dc8a4710edb1
satishck1992 <satishck1992@gmail.com>
2016-01-18 13:52:01 +0530

    Resolved bugs

--- src/ejabberd_sm_mnesia.erl
+++ src/ejabberd_sm_mnesia.erl
@@ -28,7 +28,7 @@
 -include("ejabberd_sm.hrl").
 -include("jlib.hrl").
 -include_lib("stdlib/include/ms_transform.hrl").
--include("mod_away_message.hrl").
+-include("mod_unreceived_message.hrl").
 
 -record(state, {}).
 
@@ -93,9 +93,9 @@ init([]) ->
     mnesia:add_table_copy(session, node(), ram_copies),
     mnesia:add_table_copy(session_counter, node(), ram_copies),
     mnesia:subscribe(system),
-    mnesia:create_table(away_message,
+    mnesia:create_table(unreceived_message,
       [{disc_copies, [node()]}, {type, bag},
-      {attributes, record_info(fields, away_message)}]),
+      {attributes, record_info(fields, unreceived_message)}]),
     {ok, #state{}}.
 
 handle_call(_Request, _From, State) ->
