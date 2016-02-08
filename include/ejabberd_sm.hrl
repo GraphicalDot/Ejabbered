947bfa04a93f67c230d442b7a767cf44290b4bf3
satishck1992 <satishck1992@gmail.com>
2016-02-08 12:01:30 +0530

    Removed some changes

--- include/ejabberd_sm.hrl
+++ include/ejabberd_sm.hrl
@@ -3,7 +3,6 @@
 
 -record(session, {sid, usr, us, priority, info}).
 -record(session_counter, {vhost, count}).
--record(user_session, {vhost, count}).
 -type sid() :: {erlang:timestamp(), pid()}.
 -type ip() :: {inet:ip_address(), inet:port_number()} | undefined.
 -type info() :: [{conn, atom()} | {ip, ip()} | {node, atom()}
